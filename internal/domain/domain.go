40081049ba241a8b88a0bed70ead580e6725eb30
Daniil Rutskiy <dstdfx@gmail.com>
2021-11-06 22:58:22 +0300

    Lint code + add comments to domain entities.
    Signed-off-by: Daniil Rutskiy <dstdfx@gmail.com>

--- internal/domain/domain.go
+++ internal/domain/domain.go
@@ -12,24 +12,29 @@ type EventProvider interface {
 	Type() EventProviderType
 }
 
-type EventsHandler interface {
-}
+type EventsHandler interface{}
 
+// EventType represents an event type.
 type EventType string
 
 const (
-	StartEventType EventType = "start"
-	LoginEventType EventType = "login"
+	StartEventType       EventType = "start"
+	LoginEventType       EventType = "login"
 	TextMessageEventType EventType = "text_message" // whatsapp only
-	ReplyEventType EventType = "reply" // telegram only
+	ReplyEventType       EventType = "reply"        // telegram only
 )
 
+// Event represents a generic event API.
 type Event interface {
 	Type() EventType
 }
 
+// StartEvent represents an initial event.
 type StartEvent struct {
+	// ChatID is telegram bot chat identifier.
 	ChatID int64
+
+	// FromUser is a telegram username of the client that interacts with the bot.
 	FromUser string
 }
 
@@ -37,8 +42,12 @@ func (se *StartEvent) Type() EventType {
 	return StartEventType
 }
 
+// LoginEvent represents a login event.
 type LoginEvent struct {
+	// ChatID is telegram bot chat identifier.
 	ChatID int64
+
+	// FromUser is a telegram username of the client that interacts with the bot.
 	FromUser string
 }
 
@@ -46,9 +55,15 @@ func (le *LoginEvent) Type() EventType {
 	return LoginEventType
 }
 
+// TextMessageEvent represents an incoming text message event.
 type TextMessageEvent struct {
-	WhatsappRemoteJid  string
+	// WhatsappRemoteJid is a whatsapp client identifier that sent the message.
+	WhatsappRemoteJid string
+
+	// WhatsappSenderName is a whatsapp client's name that sent the message.
 	WhatsappSenderName string
+
+	// Text is a text message body.
 	Text string
 }
 
@@ -56,10 +71,17 @@ func (te *TextMessageEvent) Type() EventType {
 	return TextMessageEventType
 }
 
+// ReplyEvent represents a message reply event.
 type ReplyEvent struct {
+	// ChatID is telegram bot chat identifier.
 	ChatID int64
+
+	// FromUser is a telegram username of the client that interacts with the bot.
 	FromUser string
+
+	// Reply is a reply text message body.
 	Reply string
+
 	// TODO: add some identifier to know whom to reply
 }
 
