c0f61151e25be634618bb72826a72e12e5d816fc
Daniil Rutskiy <dstdfx@gmail.com>
2021-11-06 23:05:03 +0300

    Lint code + comments telegram provider
    Signed-off-by: Daniil Rutskiy <dstdfx@gmail.com>

--- internal/telegram/events_provider.go
+++ internal/telegram/events_provider.go
@@ -8,24 +8,29 @@ import (
 	"go.uber.org/zap"
 )
 
+// EventsProvider represents telegram events provider.
 type EventsProvider struct {
 	log         *zap.Logger
 	telegramAPI *tgbotapi.BotAPI
-	eventsCh chan domain.Event
+	eventsCh    chan domain.Event
 }
 
+// Opts represents options to create new instance of EventsProvider.
 type Opts struct {
 	TelegramAPI *tgbotapi.BotAPI
 }
 
+// NewEventsProvider creates new instance of EventsProvider.
 func NewEventsProvider(log *zap.Logger, opts *Opts) *EventsProvider {
 	return &EventsProvider{
 		log:         log,
 		telegramAPI: opts.TelegramAPI,
-		eventsCh: make(chan domain.Event),
+		eventsCh:    make(chan domain.Event),
 	}
 }
 
+// Run method starts the main goroutine of EventsProvider.
+// The call is blocking.
 func (ep *EventsProvider) Run(ctx context.Context) error {
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
@@ -69,11 +74,12 @@ func (ep *EventsProvider) Run(ctx context.Context) error {
 	}
 }
 
+// EventsStream method returns a stream of domain.Event.
 func (ep *EventsProvider) EventsStream() <-chan domain.Event {
 	return ep.eventsCh
 }
 
+// Type method returns a type of the provider.
 func (ep *EventsProvider) Type() domain.EventProviderType {
 	return domain.TelegramEventProvider
 }
-
--- internal/telegram/events_provider_test.go
+++ internal/telegram/events_provider_test.go
@@ -1,3 +1,10 @@
 package telegram_test
 
+import (
+	"github.com/dstdfx/twbridge/internal/domain"
+	"github.com/dstdfx/twbridge/internal/telegram"
+)
+
+var _ domain.EventProvider = new(telegram.EventsProvider)
+
 // TODO: to be added
